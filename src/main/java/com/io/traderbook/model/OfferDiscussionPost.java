9da66f16f26b90f3dc93391b9b447ffd5344ce81
Desmond14 <s.lakomy@gmail.com>
2014-03-30 15:41:54 +0200

    Introduced OfferDiscussionPost class

---
+++ src/main/java/com/io/traderbook/model/OfferDiscussionPost.java
@@ -0,0 +1,56 @@
+package com.io.traderbook.model;
+
+import javax.persistence.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: slakomy
+ * Date: 3/29/14
+ * Time: 11:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "OfferDiscussionPosts")
+public class OfferDiscussionPost {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String postContent;
+    @ManyToOne
+    private User postAuthor;
+
+    protected OfferDiscussionPost() {
+
+    }
+
+    public OfferDiscussionPost(User postAuthor, String postContent) {
+        this.postAuthor = postAuthor;
+        this.postContent = postContent;
+    }
+
+    private User getPostAuthor() {
+        return postAuthor;
+    }
+
+    private void setPostAuthor(User postAuthor) {
+        this.postAuthor = postAuthor;
+    }
+
+    private String getPostContent() {
+        return postContent;
+    }
+
+    private void setPostContent(String postContent) {
+        this.postContent = postContent;
+    }
+
+    private long getId() {
+        return id;
+    }
+
+    private void setId(long id) {
+        this.id = id;
+    }
+}
